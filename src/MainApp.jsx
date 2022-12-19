95a4c39ca6e818c6f9f626670c73a284fb5067d7
Aron <blazte.aster@gmail.com>
2022-12-19 08:23:52 -0500

    add ForeCast component to MainApp.jsx component

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './sass/index.scss'
 import { CurrentWeather } from './components/CurrentWeather/CurrentWeather'
+import { getWeather } from './services/getWeather'
+import { ForeCast } from './components/ForeCast/ForeCast'
 
 export const MainApp = () => {
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition( (position) => {
+      const {latitude, longitude} = position.coords
+      // getWeather(latitude, longitude).then(data => console.log(data))
+    })
+  }, [])
   return (
     <>
       <CurrentWeather />
+      <ForeCast />
     </>
   );
 };
