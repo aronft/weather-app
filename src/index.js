745f4717f6298d2d9e7a4b30c9ab6339fa9d96d6
Aron <blazte.aster@gmail.com>
2022-12-19 08:49:35 -0500

    comment strict mode

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { MainApp } from './MainApp'
+import './sass/index.scss'
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <MainApp/>
-  </React.StrictMode>,
+  // </React.StrictMode>,
 );
