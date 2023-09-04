eef27f41cc159db914d802cfc06c9e4c04e97458
blazte <blazte.aster@gmail.com>
2023-09-04 15:47:34 -0500

    setup eslint

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 import './sass/index.scss'
 import { CurrentWeather } from './components/CurrentWeather/CurrentWeather'
 import { ForeCast } from './components/ForeCast/ForeCast'
 import './_main-app.scss'
 export const MainApp = () => {
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition( (position) => {
-      const {latitude, longitude} = position.coords
-      // getWeather(latitude, longitude).then(data => console.log(data))
-    })
-  }, [])
-  return (
-    <div className={'main-app'}>
-      <CurrentWeather />
-      <ForeCast />
-    </div>
-  );
-};
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords
+            // getWeather(latitude, longitude).then(data => console.log(data))
+        })
+    }, [])
+    return (
+        <div className={'main-app'}>
+            <CurrentWeather />
+            <ForeCast />
+        </div>
+    )
+}
